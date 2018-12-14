f68f17efc1c598f6dcdc3bcbc149ace7165679b6
kalidou-papa <kalidou2015@gmail.com>
2018-12-14 10:12:35 +0000

    3 commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ function leetspeak(chr) {
     var chrs = {'a':'4','e':'3','l':'1','t':'7','s':'5','o':'0'};
     return chrs[chr.toLowerCase()] || chr;
 }
+function french(chr) {
+    var chrs2 = {'4':'a','3':'e','1':'l','7':'t','5':'s','0':'o'};
+    return chrs2[chr.toLowerCase()] || chr;
+}
 document.getElementById("btn").addEventListener("click", function() {
     var mess = document.getElementById("mess").value;
     var result = document.getElementById("result");
 
-    console.log( mess.replace(/[aeltso]/g, leetspeak) );
-    result.innerHTML = mess.replace(/[aeltso]/g, leetspeak);
+    if ( !mess.match(/[0-9]/g) ) {
+        result.innerHTML = mess.replace(/[aeltso]/g, leetspeak);
+    } else {
+        result.innerHTML = mess.replace(/[431750]/g, french);
+    }
+
 });
+
+
+
+
